88afc45c4842f3675f6106feccdcada4f265be5d
Edward Dorelien <edwarddorelien@gmail.com>
2022-03-14 16:11:06 -0400

    heroku deploy

--- server/index.js
+++ server/index.js
@@ -20,18 +20,19 @@ app.use(bodyParser.json());
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+/*
 if (process.env.NODE_ENV === 'production'){
   app.use(express.static('./client/build'));
 }
-
+*/
 // Production Build
-/*
+
 app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
-*/
+
 //app.use('/api', itemRouter);
 app.use('/api', patientRouter);
 app.use('/api', examRouter);
