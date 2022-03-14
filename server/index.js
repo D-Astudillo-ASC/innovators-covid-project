e056f0998ab6d0390125f6bfdd32734da5986e9b
Edward Dorelien <edwarddorelien@gmail.com>
2022-03-14 18:11:41 -0400

    deploy heroku

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ const patientRouter = require('./routes/patient-router');
 const examRouter = require('./routes/exam-router');
 
 const app = express();
-const apiPort = process.env.PORT || 3000;
+//const apiPort = process.env.PORT || 3000;
+const apiPort = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -26,13 +27,13 @@ if (process.env.NODE_ENV === 'production'){
 }
 */
 // Production Build
-
+/*
 app.use(express.static(path.resolve(__dirname, './client/build')))
 
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
 });
-
+*/
 //app.use('/api', itemRouter);
 app.use('/api', patientRouter);
 app.use('/api', examRouter);
