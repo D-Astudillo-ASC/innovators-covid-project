fe5b7bc6dae224777b92c5a74e9a51ea0f55aa6b
Edward Dorelien <edwarddorelien@gmail.com>
2022-03-14 16:05:43 -0400

    heroku deploy

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+require('dotenv').config();
+
+const path = require('path');
 
 const db = require('./db');
 // const itemRouter = require('./routes/item-router');
@@ -9,7 +12,7 @@ const patientRouter = require('./routes/patient-router');
 const examRouter = require('./routes/exam-router');
 
 const app = express();
-const apiPort = 3000;
+const apiPort = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -17,11 +20,19 @@ app.use(bodyParser.json());
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+if (process.env.NODE_ENV === 'production'){
+  app.use(express.static('./client/build'));
+}
 
-// app.use('/api', itemRouter);
+// Production Build
+/*
+app.use(express.static(path.resolve(__dirname, './client/build')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+*/
+//app.use('/api', itemRouter);
 app.use('/api', patientRouter);
 app.use('/api', examRouter);
 
